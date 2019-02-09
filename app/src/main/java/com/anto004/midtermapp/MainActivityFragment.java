bb98da2e58eb20630c62f79ffbae118824781026
anto004 <anto_004@live.com>
2019-02-09 13:33:24 -0800

    Add heart UI in main screen

--- app/src/main/java/com/anto004/midtermapp/MainActivityFragment.java
+++ app/src/main/java/com/anto004/midtermapp/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.anto004.midtermapp;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,5 +23,25 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        TextView nameTV = getActivity().findViewById(R.id.name_textview);
+        Button getInfoButton = getActivity().findViewById(R.id.get_info_button);
+        ImageView tabletIV = getActivity().findViewById(R.id.tablet_imageView);
+        ImageView heartIV = getActivity().findViewById(R.id.heart_image_view);
+
+        tabletIV.setImageResource(R.drawable.xperia_z_main_image);
+
+        getInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
