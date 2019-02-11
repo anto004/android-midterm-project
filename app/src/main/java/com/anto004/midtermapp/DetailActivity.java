b68949e290b8ad893fff26aec64ffe25e6fab074
anto004 <anto_004@live.com>
2019-02-10 18:41:02 -0800

    Add back button on action bar

--- app/src/main/java/com/anto004/midtermapp/DetailActivity.java
+++ app/src/main/java/com/anto004/midtermapp/DetailActivity.java
@@ -15,6 +15,9 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
