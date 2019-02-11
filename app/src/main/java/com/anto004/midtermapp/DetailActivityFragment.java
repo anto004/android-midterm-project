537d4735a150420502b5ceaf4e84067237dd61cf
anto004 <anto_004@live.com>
2019-02-10 19:25:13 -0800

    Send opinion from detail screen to main screen

--- app/src/main/java/com/anto004/midtermapp/DetailActivityFragment.java
+++ app/src/main/java/com/anto004/midtermapp/DetailActivityFragment.java
@@ -1,5 +1,6 @@
 package com.anto004.midtermapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,8 +10,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,6 +29,7 @@ public class DetailActivityFragment extends Fragment {
     public static final String HEIGHT = "height";
     public static final String WIDTH = "width";
     public static final String DETAIL_IMAGE = "detailImage";
+    public static final String OPINION = "opinion";
 
 
     TextView detailNameTV;
@@ -50,8 +56,21 @@ public class DetailActivityFragment extends Fragment {
         heightTV = getActivity().findViewById(R.id.height_value_textView);
         widthTV = getActivity().findViewById(R.id.width_value_textView);
         detailIV = getActivity().findViewById(R.id.detail_image);
+        Button doneButton = getActivity().findViewById(R.id.done_button);
+        final CheckBox likeCheckBox = getActivity().findViewById(R.id.like_checkbox);
+
 
         setValuesFromIntent();
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra(OPINION, likeCheckBox.isChecked());
+                getActivity().setResult(RESULT_OK, data);
+                getActivity().finish();
+            }
+        });
 
     }
 
--- app/src/main/java/com/anto004/midtermapp/MainActivityFragment.java
+++ app/src/main/java/com/anto004/midtermapp/MainActivityFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    public static final String LOG_TAG = "MainActivityFragment";
+    public static final int MAINSCREEN_CODE = 100;
 
     public MainActivityFragment() {
     }
@@ -45,8 +51,21 @@ public class MainActivityFragment extends Fragment {
                 intent.putExtra(DetailActivityFragment.WIDTH, "1920");
                 intent.putExtra(DetailActivityFragment.HEIGHT, "1200");
                 intent.putExtra(DetailActivityFragment.DETAIL_IMAGE, R.drawable.xperia_z_detail_image_copy);
-                startActivity(intent);
+                startActivityForResult(intent, MAINSCREEN_CODE);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == MAINSCREEN_CODE){
+            if(resultCode == RESULT_OK){
+                Bundle bundle = data.getExtras();
+                boolean opinion = bundle.getBoolean(DetailActivityFragment.OPINION);
+                Log.d(LOG_TAG, "data: "+ opinion);
+            }
+        }
     }
 }
